6b5f78432dd6b390acb6ccc402a71977fd624434
rasim99 <rasimjm@code.edu.az>
2022-11-20 03:32:43 -0800

    task

---
+++ Person/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    class Person
+    {
+        private string name;
+        public string  Name
+        {
+             get
+             {
+               
+                return " " ;
+             }
+             set
+             {
+                if (value.Length>=5)
+                {
+                    name = value;
+                    Console.WriteLine("set is okey");
+                    return;
+                }
+                Console.WriteLine("more length name add");
+             }
+        }
+       
+
+
+
+    } 
+}
---
+++ Person/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Person = new Person();
+            Person.Name = "Lolahhh";
+            Console.WriteLine(Person.Name);
+            
+
+            
+        }
+    }
+}
